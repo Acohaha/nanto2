e3fd911edc951ac80f685dbd506bef63ec8a763e
Acohaha <114286230+Acohaha@users.noreply.github.com>
2023-01-09 07:42:21 +0800

    Add files via upload

---
+++ form.js
@@ -0,0 +1,16 @@
+const form=document.querySelector("form"),
+      nextBtn=form. querySelector(".nextBtn"),
+      backBtn=form.querySelector(".backBtn"),
+      allInput=form.querySelector(".first input");
+
+nextBtn.addEventListener("click", ()=>{
+    allInput.forEach(allInput=>{
+        form.classList.add('secActive');
+    }else{
+        form.classList.remove('secActive');
+        }
+    })
+})
+
+backBtn.addEventListener("click",()==>
+form.classList.remove('secActive'));
